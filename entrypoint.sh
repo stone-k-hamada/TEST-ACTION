2dfffd32faa475aac74414e09aed914b87df7b0b
stone-k-hamada <57883168+stone-k-hamada@users.noreply.github.com>
2019-12-03 16:42:04 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "start"
 sshpass -p $AWS_PASS ssh -t -o StrictHostKeyChecking=no $AWS_LOGIN
 
 sshpass -p $AWS_PASS ssh $AWS_LOGIN bash -c "'
-mkdir /test/AAA
+sudo mkdir /test/AAA
 touch /test/TESTTEST
 '"
 
