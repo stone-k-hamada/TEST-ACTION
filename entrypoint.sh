be80bfdb0a9ceeec180a1be1f3d365496761965f
stone-k-hamada <57883168+stone-k-hamada@users.noreply.github.com>
2019-12-04 09:49:18 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -l
 
-echo "start"
+echo "Start"
 
-echo "create a zip file from my github repository"
+echo "Create a zip file from my github repository."
 cd ./test && ls -a
-zip -r ./test/dist.zip ./ -x \*/.git/\* \*/.github/\*
-echo "zip file created"
+zip -r ./test/dist.zip ./
+# -x \*/.git/\* \*/.github/\*
+echo "zip file created."
 
 
-echo "start transporting files"
+echo "Start transporting zip file."
 sshpass -p $AWS_PASS scp -t -o StrictHostKeyChecking=no ./dist.zip ${AWS_LOGIN}:/test
+echo "transported zip file."
 
 sshpass -p $AWS_PASS ssh $AWS_LOGIN bash -c "'
 sudo mkdir /test
