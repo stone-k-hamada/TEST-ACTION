e8afb897db8f1b06f6134c3b8b82608a07285ded
stone-k-hamada <57883168+stone-k-hamada@users.noreply.github.com>
2019-12-04 10:59:22 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "zip file created."
 
 
 echo "Start transporting zip file."
-sshpass -p $AWS_PASS scp -t -o StrictHostKeyChecking=no ./dist.zip ${AWS_LOGIN}:/test
+sshpass -p $AWS_PASS scp -t -o StrictHostKeyChecking=no ./test/dist.zip ${AWS_LOGIN}:/test
 echo "transported zip file."
 
 sshpass -p $AWS_PASS ssh $AWS_LOGIN bash -c "'
