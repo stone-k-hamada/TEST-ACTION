eef121cea90eb77d745c51222b56e17218c31be7
stone-k-hamada <57883168+stone-k-hamada@users.noreply.github.com>
2019-12-04 09:17:29 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@
 
 echo "start"
 
+echo "create a zip file from my github repository"
+cd / && ls -a
+zip -r /dist.zip / -x \*/.git/\* \*/.github/\*
+cd / && ls -a
+
 sshpass -p $AWS_PASS ssh -t -o StrictHostKeyChecking=no $AWS_LOGIN
 
 sshpass -p $AWS_PASS ssh $AWS_LOGIN bash -c "'
